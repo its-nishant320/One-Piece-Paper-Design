986c19875205a76bcb9dcb92fbb0172dec1d7758
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-09-09 03:39:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl = gsap.timeline()
+
+tl.to("#page1",{
+    y:"120vh",
+    scale:0.6,
+    duration:0
+})
+
+tl.to("#page1",{
+    y:"30vh",
+    z:"50vh",
+    duration:1,
+    delay:1,
+    ease:"slow"
+});
+
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:2,
+    ease:"slow"
+});
